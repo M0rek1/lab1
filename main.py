fbea043d3576e2c8409e90a8183ca65482b4fb16
M0rek1 <s28214@pjwstk.edu.pl>
2024-03-01 15:36:24 +0100

    lab1

--- main.py
+++ main.py
@@ -1,16 +1,130 @@
-# This is a sample Python script.
+import os
+import turtle as t
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+playerAscore = 0
+playerBscore = 0
+
+# create a window and declare a variable called window and call the screen()
+window = t.Screen()
+window.title("The Pong Game")
+window.bgcolor("green")
+window.setup(width=800, height=600)
+window.tracer(0)
+
+# Creating the left paddle
+leftpaddle = t.Turtle()
+leftpaddle.speed(0)
+leftpaddle.shape("square")
+leftpaddle.color("white")
+leftpaddle.shapesize(stretch_wid=5, stretch_len=1)
+leftpaddle.penup()
+leftpaddle.goto(-350, 0)
+
+# Creating the right paddle
+rightpaddle = t.Turtle()
+rightpaddle.speed(0)
+rightpaddle.shape("square")
+rightpaddle.color("white")
+rightpaddle.shapesize(stretch_wid=5, stretch_len=1)
+rightpaddle.penup()
+rightpaddle.goto(-350, 0)
+
+# Code for creating the ball
+ball = t.Turtle()
+ball.speed(0)
+ball.shape("circle")
+ball.color("red")
+ball.penup()
+ball.goto(5, 5)
+ballxdirection = 0.2
+ballydirection = 0.2
+
+# Code for creating pen for scorecard update
+pen = t.Turtle()
+pen.speed(0)
+pen.color("Blue")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("score", align="center", font=('Arial', 24, 'normal'))
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# code for moving the leftpaddle
+def leftpaddleup():
+    y = leftpaddle.ycor()
+    y = y + 90
+    leftpaddle.sety(y)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def leftpaddledown():
+    y = leftpaddle.ycor()
+    y = y + 90
+    leftpaddle.sety(y)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# code for moving the rightpaddle
+def rightpaddleup():
+    y = rightpaddle.ycor()
+    y = y + 90
+    rightpaddle.sety(y)
+
+
+def rightpaddledown():
+    y = rightpaddle.ycor()
+    y = y + 90
+    rightpaddle.sety(y)
+
+
+# Assign keys to play
+window.listen()
+window.onkeypress(leftpaddleup, 'w')
+window.onkeypress(leftpaddledown, 's')
+window.onkeypress(rightpaddleup, 'Up')
+window.onkeypress(rightpaddledown, 'Down')
+
+while True:
+    window.update()
+
+    # moving the ball
+    ball.setx(ball.xcor() + ballxdirection)
+    ball.sety(ball.ycor() + ballxdirection)
+
+    # border set up
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ballydirection = ballydirection * -1
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ballydirection = ballydirection * -1
+
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball_dx = ball_dx * -1
+        player_a_score = player_a_score + 1
+        pen.clear()
+        pen.write("Player A: {}                    Player B: {} ".format(player_a_score, player_b_score),
+                  align="center", font=('Monaco', 24, "normal"))
+        os.system("afplay wallhit.wav&")
+
+    if (ball.xcor()) < -390:  # Left width paddle Border
+        ball.goto(0, 0)
+        ball_dx = ball_dx * -1
+        player_b_score = player_b_score + 1
+        pen.clear()
+        pen.write("Player A: {}                    Player B: {} ".format(player_a_score, player_b_score),
+                  align="center", font=('Monaco', 24, "normal"))
+        os.system("afplay wallhit.wav&")
+
+    # Handling the collisions with paddles.
+
+    if (ball.xcor() > 340) and (ball.xcor() < 350) and (
+            ball.ycor() < rightpaddle.ycor() + 40 and ball.ycor() > rightpaddle.ycor() - 40):
+        ball.setx(340)
+        ball_dx = ball_dx * -1
+        os.system("afplay paddle.wav&")
+
+    if (ball.xcor() < -340) and (ball.xcor() > -350) and (
+            ball.ycor() < leftpaddle.ycor() + 40 and ball.ycor() > leftpaddle.ycor() - 40):
+        ball.setx(-340)
+        ball_dx = ball_dx * -1
+        os.system("afplay paddle.wav&")
